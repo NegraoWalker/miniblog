faf6edd4fec18f9e68eaf744ab0da4ce1fa32df8
walkerNegrão <walker.esteves.negrao@gmail.com>
2022-09-03 20:00:12 -0300

    Adicionando o navbar e footer na aplicação

--- src/App.js
+++ src/App.js
@@ -1,19 +1,25 @@
 import './App.css'
 import { BrowserRouter, Routes, Route, Navigate } from "react-router-dom"
+//*Pages:
 import Home from './pages/Home/Home';
 import About from './pages/About/About';
+//*Components:
+import Footer from './components/Footer';
+import Navbar from './components/Navbar';
 
 
 function App() {
   return (
     <div className="App">
       <BrowserRouter>
+        <Navbar />
         <div className="container">
           <Routes>
             <Route path='/' element={<Home />}></Route>
             <Route path='/about' element={<About />}></Route>
           </Routes>
         </div>
+        <Footer />
       </BrowserRouter>
     </div>
   );
