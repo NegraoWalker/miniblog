07b6bd9cb0bc0d3b99085330508432504db0d8fc
walkerNegrão <walker.esteves.negrao@gmail.com>
2022-09-05 20:35:42 -0300

    Adicionando as páginas de login e register no navbar

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import './App.css'
 import { BrowserRouter, Routes, Route, Navigate } from "react-router-dom"
-//*Pages:
-import Home from './pages/Home/Home';
-import About from './pages/About/About';
 //*Components:
 import Footer from './components/Footer';
 import Navbar from './components/Navbar';
+//*Pages:
+import Home from './pages/Home/Home';
+import About from './pages/About/About';
+import Login from './pages/Login/Login';
+import Register from './pages/Register/Register';
 
 
 function App() {
@@ -17,6 +19,8 @@ function App() {
           <Routes>
             <Route path='/' element={<Home />}></Route>
             <Route path='/about' element={<About />}></Route>
+            <Route path='/login' element={<Login />}></Route>
+            <Route path='/register' element={<Register />}></Route>
           </Routes>
         </div>
         <Footer />
--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -14,6 +14,19 @@ const Navbar = () => {
                         Home
                     </NavLink>
                 </li>
+
+                <li>
+                    <NavLink to='/login' className={({ isActive }) => (isActive ? styles.active : "")}>
+                        Login
+                    </NavLink>
+                </li>
+
+                <li>
+                    <NavLink to='/register' className={({ isActive }) => (isActive ? styles.active : "")}>
+                        Register
+                    </NavLink>
+                </li>
+
                 <li>
                     <NavLink to='/about' className={({ isActive }) => (isActive ? styles.active : "")}>
                         About
