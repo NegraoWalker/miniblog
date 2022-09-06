7e34d9b96bede5abd5a3d3f5f30120a3c6b0a869
walkerNegrão <walker.esteves.negrao@gmail.com>
2022-09-05 21:08:42 -0300

    Adicionando CSS para a página register

--- src/pages/Register/Register.js
+++ src/pages/Register/Register.js
@@ -1,10 +1,35 @@
 import React from 'react'
 import styles from "./Register.module.css"
 
+import { useState, useEffect } from 'react'
+
 const Register = () => {
     return (
-        <div>
-            <h1>Register</h1>
+        <div className={styles.register}>
+            <h1>Register user</h1>
+            <form>
+                <label>
+                    <span>Name:</span>
+                    <input type="text" name="displayName" required placeholder='Username' />
+                </label>
+
+                <label>
+                    <span>E-mail:</span>
+                    <input type="email" name="email" required placeholder='User e-mail' />
+                </label>
+
+                <label>
+                    <span>Password:</span>
+                    <input type="password" name="password" required placeholder='Enter password' />
+                </label>
+
+                <label>
+                    <span>Confirm password:</span>
+                    <input type="password" name="confirmPassword" required placeholder='Confirm your password' />
+                </label>
+
+                <button type="submit" className='btn'>Register</button>
+            </form>
         </div>
     )
 }
